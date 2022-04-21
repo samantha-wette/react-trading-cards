1cf9ba626015659639dda59d08b7aa51d819861d
Samantha Wette <samantha.wette@gmail.com>
2022-04-21 12:08:43 -0700

    trading cards formatted

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <React.Fragment>
+      <l>Hey there!</l>
+      <a href='/cards'>The cards are here!</a>
+      <img src='/static/img/balloonicorn.jpg'></img>      
+    </React.Fragment>
+  )
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
